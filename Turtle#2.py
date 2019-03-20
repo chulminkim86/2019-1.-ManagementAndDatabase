622fbb3e1836ad91c358a096f8452a22539a5958
chulmin.kim <friend100n@gmail.com>
2019-03-20 18:16:21 +0900

    Update Turtle#2.py

--- Turtle#2.py
+++ Turtle#2.py
@@ -2,13 +2,13 @@
 
 #랜덤으로 움직이는 거북이
 
-$ import turtle as t #turtle 패키지 호출
-$ import random #random 패키지 호출
+import turtle as t #turtle 패키지 호출
+import random #random 패키지 호출
 
-$ t.shape("turtle") #turtle 모양 설정
-$ t.speed(3) #속도 3
+t.shape("turtle") #turtle 모양 설정
+t.speed(3) #속도 3
 
-$ for x in range(500): 
+for x in range(500): 
       a = random.randint(1, 360)
       t.setheading(a)
       t.forward(10)
